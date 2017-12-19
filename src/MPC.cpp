06fa2fd357385648513718759926ad9df22d4d74
mdf3039 <mdf3039@gmail.com>
2017-12-19 13:25:45 -0600

    52nd - changed parameters

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -62,20 +62,20 @@ class FG_eval {
     // any anything you think may be beneficial.
     // The part of the cost based on the reference state.
     for (size_t t = 0; t < N; t++) {
-      fg[0] += 7*CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 4*CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 1700*CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 1400*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += 100*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
     for (size_t t = 0; t < N - 1; t++) {
-      fg[0] += CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
     for (size_t t = 0; t < N - 2; t++) {
-      fg[0] += 5*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
