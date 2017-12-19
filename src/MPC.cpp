49088ad9df7d37a7041316a1df8329b653f347d3
mdf3039 <mdf3039@gmail.com>
2017-12-19 15:12:08 -0600

    59th - going to submit. Changed lack back to 0.1

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -7,7 +7,7 @@ using CppAD::AD;
 
 // TODO: Set the timestep length and duration
 size_t N = 100;
-double dt = 0.2;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
