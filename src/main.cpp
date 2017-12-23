f7ff2ff5e9c307987e23f4ed3fcd4529d8c0b0cd
mdf3039 <mdf3039@gmail.com>
2017-12-23 09:54:54 -0600

    Want to observe peak velocity at throttle=0.1

--- src/main.cpp
+++ src/main.cpp
@@ -197,7 +197,7 @@ int main() {
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = -0.05;//steer_value/deg2rad(25);
+          msgJson["steering_angle"] = -0.03;//steer_value/deg2rad(25);
           msgJson["throttle"] = .1;//throttle_value;
 
           //Display the MPC predicted trajectory
