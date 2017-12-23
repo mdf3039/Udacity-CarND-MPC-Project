c216502b5afb3368101e19eb9108900db1e6287a
mdf3039 <mdf3039@gmail.com>
2017-12-23 12:02:39 -0600

    differential delta_t=30,000

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -75,7 +75,7 @@ class FG_eval {
 
     // Minimize the value gap between sequential actuations.
     for (size_t t = 0; t < N - 2; t++) {
-      fg[0] += 15000*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 30000*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += 2500*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
