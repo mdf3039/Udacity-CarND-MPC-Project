445536c6f217ed0517a9abb2da59cb3ac31ceb9e
mdf3039 <mdf3039@gmail.com>
2017-12-23 12:29:23 -0600

    acc=10, changed the latency update equation

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -70,7 +70,7 @@ class FG_eval {
     // Minimize the use of actuators.
     for (size_t t = 0; t < N - 1; t++) {
       fg[0] += 15000*CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 100*CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 10*CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
